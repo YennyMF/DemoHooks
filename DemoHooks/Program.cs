5643db227414c15c2b7d89c37a84c4dd7df4df1f
Yenny Menor Fernández <yenymenor18@gmail.com>
2024-11-28 11:22:49 -0500

    Demostración de formato de Hooks  de código

--- DemoHooks/Program.cs
+++ DemoHooks/Program.cs
@@ -1,2 +1,12 @@
 ﻿
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Prueba de formateo de código");
+int edad = 28;
+if (edad >= 18)
+    {
+       Console.WriteLine("Es mayor de edad");
+        }
+    else
+    {
+           Console.WriteLine("Es menor de edad");
+           }
